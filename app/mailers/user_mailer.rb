aeb73cfd78350a44fec53fc5dff46650aa637087
Danil Pismenny <danil@brandymint.ru>
2018-11-02 15:32:38 +0300

    require master key for production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,18 +3,18 @@ class UserMailer < ApplicationMailer
     @user = user
     @activation_url =  activate_user_url(id: user.activation_token || raise("User #{user.id} has no activation_token"))
 
-    mail(to: user.email, subject: I18n.t('emails.activation.subject'))
+    mail(to: user.email, subject: I18n.t('email.activation.subject'))
   end
 
   def activation_success_email(user)
     @user = user
-    mail(to: user.email, subject: I18n.t('emails.activation_success.subject'))
+    mail(to: user.email, subject: I18n.t('email.activation_success.subject'))
   end
 
   def reset_password_email(user)
     @user = user
     @reset_password_url = edit_password_reset_url(@user.reset_password_token || raise("User #{user.id} has no reset_password_token"))
 
-    mail(to: user.email, subject: I18n.t('emails.reset_password.subject'))
+    mail(to: user.email, subject: I18n.t('email.reset_password.subject'))
   end
 end
