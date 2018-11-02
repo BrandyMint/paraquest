b8415e84a689f55d21b828616a24dbe0be8981a5
Danil Pismenny <danil@brandymint.ru>
2018-11-02 18:12:12 +0300

    Обработка ActionController::InvalidAuthenticityTokenэ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pagination
+  rescue_from ActionController::InvalidAuthenticityToken, with: :rescue_invalid_authenticity_token
 
   private
+
+  def rescue_invalid_authenticity_token
+    render 'user_sessions/new', locals: {
+      user_session: UserSession.new,
+      message:  'Просрочен токен аутентификации, авторизуйтесь снова',
+    }, layout: 'simple'
+  end
 
   def t_flash(key, options = {})
     I18n.t key, options.merge(scope: [:flashes])
